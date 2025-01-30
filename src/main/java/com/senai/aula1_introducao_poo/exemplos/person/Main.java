c3bb3d3aba50e16ddf50fa48bf56924c5e1807e8
Aluno <joelcolodro@gmail.com>
2025-01-30 11:13:17 -0300

    commit 30/01

---
+++ src/main/java/com/senai/aula1_introducao_poo/exemplos/person/Main.java
@@ -0,0 +1,10 @@
+package com.senai.aula1_introducao_poo.exemplos.person;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Person person1 = new Person();
+        person1.name = "Joel";
+        System.out.println(person1.name);
+    }
+}
---
+++ src/main/java/com/senai/aula1_introducao_poo/exemplos/person/Person.java
@@ -0,0 +1,16 @@
+package com.senai.aula1_introducao_poo.exemplos.person;
+
+public class Person {
+    String name;
+    int age;
+    float height;
+    String endereço;
+
+    public void falar(String frase){
+        System.out.println(frase);
+    }
+
+    public void comer(String alimento){
+        System.out.println("a pessoa comeu: "+ alimento);
+    }
+}

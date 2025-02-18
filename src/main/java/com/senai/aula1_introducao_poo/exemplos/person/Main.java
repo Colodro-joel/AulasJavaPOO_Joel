d2d8631865e79930b32029d0513cf496e1a0916e
Aluno <joelcolodro@gmail.com>
2025-02-18 11:11:15 -0300

    Commit 18/02

--- src/main/java/com/senai/aula1_introducao_poo/exemplos/person/Main.java
+++ src/main/java/com/senai/aula1_introducao_poo/exemplos/person/Main.java
@@ -56,6 +56,6 @@ public class Main {
                         dados[7]
                   )
         );
-                System.out.println(person3J);
+                System.out.println(person);
     }
 }

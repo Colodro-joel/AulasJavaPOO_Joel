ba3df1b408ba042deb89749e63f1e85817806e0d
Aluno <joelcolodro@gmail.com>
2025-05-08 08:43:46 -0300

    COMMIT 07/05

--- src/main/java/com/senai/aula7_mvc/crud_Usuário/Model/OperadorDAO.java
+++ src/main/java/com/senai/aula7_mvc/crud_Usuário/Model/OperadorDAO.java
@@ -1,8 +1,13 @@
 package com.senai.aula7_mvc.crud_Usuário.Model;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
 import java.io.FileReader;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.List;
 
 public class OperadorDAO{
 private final String FILE_PATH = "operador.json";
@@ -10,7 +15,13 @@ private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
 private List<Operador> carregar(){
     try(FileReader reader = new FileReader(FILE_PATH)){
-        Type listType = new TypeToken<ArrayList<Operador>>()
+        Type listType = new TypeToken<ArrayList<Operador>>() {}.getType();
+        return gson.fromJson(reader, listType);
+    } catch (Exception e) {
+        return new ArrayList<>();
     }
+}
+    public OperadorDAO (){
+        peradores = carregar();
     }
 }
